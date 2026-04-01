aa63d80f7a886f70961c93bcfd8ca198de1c6fb6
TumharaGitHubName <hassaanali994@gmail.com>
2026-04-01 15:49:48 +0500

    fix: correct input ID for name in signUp function and add newline at end of file

--- next.js
+++ next.js
@@ -1,7 +1,7 @@
 var users = [];
 
 function signUp(){
-    var nam = document.getElementById("name").value;
+    var nam = document.getElementById("nam").value;
     var email = document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
     
@@ -11,4 +11,19 @@ function signUp(){
         password: pass
     })
     alert("signup success")
+}
+
+function login(){
+    var lEmail = document.getElementById("lEmail");
+    var lPass = document.getElementById("lPass");
+    
+    var user = users.find(s => 
+        s.email === lEmail.value && s.password === lPass.value
+    )
+    if(user){
+        alert("login success")
+        window.location.href = "index.html"
+    }else{
+        alert("invalid email or password")
+    }
 }
